58ea47adeeb4167654b3cd520ffc05b384f95658
Prabhsharan Singh Brar <PrabhBrar@users.noreply.github.com>
2016-01-21 19:13:44 -0800

    Update calculator.rb

--- calculator.rb
+++ calculator.rb
@@ -6,41 +6,23 @@ puts "-- Type 'divide' to divide two numbers."
 
 choice = gets.chomp.downcase
 
+puts "Enter the first number: "
+first_num = gets.chomp.to_f
+
+puts "Enter the second number: "
+second_num = gets.chomp.to_f
+
 case choice
 	when "add"
-		puts "Enter the first number: "
-		first_num = gets.chomp.to_f
-
-		puts "Enter the second number: "
-		second_num = gets.chomp.to_f
-		
 		result = first_num + second_num
 		puts"Result: #{result}"
 	when "subtract"
-		puts "Enter the first number: "
-		first_num = gets.chomp.to_f
-
-		puts "Enter the second number: "
-		second_num = gets.chomp.to_f
-		
 		result = first_num - second_num
 		puts"Result: #{result}"
 	when "multiply"
-		puts "Enter the first number: "
-		first_num = gets.chomp.to_f
-
-		puts "Enter the second number: "
-		second_num = gets.chomp.to_f
-		
 		result = first_num * second_num
 		puts"Result: #{result}"
 	when "divide"
-		puts "Enter the first number: "
-		first_num = gets.chomp.to_f
-
-		puts "Enter the second number: "
-		second_num = gets.chomp.to_f
-
 		result = first_num / second_num
 		puts"Result: #{result}"
 	else
